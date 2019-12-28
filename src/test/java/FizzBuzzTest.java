6103633ed9378214dea2c8e082e0e6a5685fae7d
lixiaoqiang <474949281@qq.com>
2019-12-28 11:58:39 +0800

    refactor

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,19 +1,35 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(Parameterized.class)
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
 
-    private int[] number = {3,5,15};
-    private String[] expected={"Fizz","Buzz","FizzBuzz"};
-    @Test
-    public void testMultipleOf3() {
-        for(int i =0;i<number.length;i++){
-            assertEquals(expected[i], FizzBuzz.getFizzBuzz(number[i]));
-        }
+    private int num;
+    private String expected;
 
+    public FizzBuzzTest(int num, String expected) {
+        this.num = num;
+        this.expected = expected;
     }
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3, "Fizz"},
+                {5, "Buzz"},
+                {15, "FizzBuzz"}
+        });
+    }
+
+    @Test
+    public void fizzBuzzTest() {
+        assertEquals(expected,FizzBuzz.getFizzBuzz(num));
+    }
 
 }
