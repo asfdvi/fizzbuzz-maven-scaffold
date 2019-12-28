9c424b23f595f28e3e17ca505c19f9c32a5847db
lixiaoqiang <474949281@qq.com>
2019-12-28 11:57:06 +0800

    pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,14 +2,14 @@
 public class FizzBuzz {
 
     public static String getFizzBuzz(int num) {
+        if(num % 15 == 0) {
+            return "FizzBuzz";
+        }
         if(num % 3 == 0) {
             return "Fizz";
         }
         if(num % 5 == 0) {
             return "Buzz";
-        }
-        if(num % 15 == 0) {
-            return "FizzBuzz";
         }
         return String.valueOf(num);
     }
